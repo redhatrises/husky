9e3899b803753f1fdc7cf78cfecaf74ab811b185
Gabe <redhatrises@gmail.com>
2018-03-09 13:12:30 -0700

    UI Updates and dir structure

--- scapwriter/launcher.py
+++ scapwriter/launcher.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QFileSystemModel, QTreeView, QWidget, QVBoxLayout, QFileDialog, QMessageBox, QApplication
 from PyQt5.QtGui import QIcon
-from MainWindow import Ui_MainWindow
+from ui.MainWindow import Ui_MainWindow
 import sys
 import os
 import yaml
@@ -32,27 +32,36 @@ def user_home():
 
 
 def load_yaml_to_gui(self, path):
-    if path.endswith(".rule") or path.endswith(".group"):
-        self.ui.tabWidget.addTab(self.ui.tab, os.path.basename(path))
+    if path.endswith(".rule"):
+        self.ui.tabWidget.addTab(self.ui.tabRule, os.path.basename(path))
 #        self.ui.tabWidget.setTabText(self.ui.tabWidget.indexOf(self.ui.tab), "Tab 1")
 #        self.ui.tabWidget.addTab(self.ui.tab, ".rule2")
-        self.tab_2 = QtWidgets.QWidget()
-        self.tab_2.setObjectName("tab_2")
-        self.ui.tabWidget.addTab(self.tab_2, "")
+#        self.tab_2 = QtWidgets.QWidget()
+#       self.tab_2.setObjectName("tab_2")
+#        self.ui.tabWidget.addTab(self.tab_2, "")
         rule = open_yaml(path)
 #        print rule
-        self.ui.txtProfileTitle.setText(rule['title'])
-        self.ui.txtProfileDesc.setPlainText(rule['description'])
+        self.ui.txtRuleTitle.setText(rule['title'])
+        self.ui.txtRuleDesc.setPlainText(rule['description'])
         self.ui.severityComboBox.setCurrentText(rule['severity'].title())
         self.ui.txtOCILclause.setText(rule['ocil_clause'])
         self.ui.txtOCIL.setPlainText(rule['ocil'])
         self.ui.txtRationale.setPlainText(rule['rationale'])
-    self.ui.txtProfileDesc.textChanged.connect(self.onChange)
-    self.ui.txtProfileTitle.textChanged.connect(self.onChange)
-    self.ui.severityComboBox.activated.connect(self.onChange)
-    self.ui.txtOCIL.textChanged.connect(self.onChange)
-    self.ui.txtOCIL.textChanged.connect(self.onChange)
-    self.ui.txtRationale.textChanged.connect(self.onChange)
+ 
+        self.ui.txtRuleDesc.textChanged.connect(self.onChange)
+        self.ui.txtRuleTitle.textChanged.connect(self.onChange)
+        self.ui.severityComboBox.activated.connect(self.onChange)
+        self.ui.txtOCIL.textChanged.connect(self.onChange)
+        self.ui.txtOCIL.textChanged.connect(self.onChange)
+        self.ui.txtRationale.textChanged.connect(self.onChange)
+
+    if path.endswith(".group"):
+        self.ui.tabWidget.addTab(self.ui.tabGroup, os.path.basename(path))
+        group = open_yaml(path)
+        self.ui.txtGroupTitle.setText(group['title'])
+        self.ui.txtGroupDesc.setPlainText(group['description'])
+#        self.ui.txtGroupDesc.textChanged.connect(self.onChange)
+#        self.ui.txtGroupTitle.textChanged.connect(self.onChange)
 
 
 def setDirectory(self, directory):
@@ -68,38 +77,56 @@ class ApplicationWindow(QtWidgets. QMainWindow):
         self.ui.setupUi(self)
         self.ui.retranslateUi(self) 
 
-        self.model = QFileSystemModel()
-        self.proxyModel = QtCore.QSortFilterProxyModel(self.ui.treeView)
+        self.ui.tabWidget.clear()
+        [self.ui.severityComboBox.addItem(sevs) for sevs in severity]
 
-        self.model.setRootPath("")
-        self.ui.treeView.setModel(self.model)
-        self.ui.treeView.setRootIndex(self.model.index(user_home()))
+
+        self.model = QFileSystemModel()
+        self.model.setRootPath(QtCore.QDir().rootPath())
+        self.model.setReadOnly(False)
+        source = self.model.index(user_home())
+
+        self.proxyModel = QtCore.QSortFilterProxyModel(self)
         self.proxyModel.setSourceModel(self.model)
         self.proxyModel.setDynamicSortFilter(True)
-        #self.ui.treeView.setModel(self.proxyModel)
-        self.ui.treeView.setRootIndex(self.model.index(user_home()))
-        self.ui.treeView.setModel(self.model)
+        index = self.proxyModel.mapFromSource(source)
+        
+        self.ui.treeView.setModel(self.proxyModel)
+        self.ui.treeView.setRootIndex(index)
 
-        self.ui.treeView.setColumnHidden(1, True)
-        self.ui.treeView.setColumnHidden(2, True)
-        self.ui.treeView.setColumnHidden(3, True)
-        self.ui.actionQuit.triggered.connect(self.close)
+#        self.ui.treeView.setRootIndex(self.model.index(user_home()))
+#        self.ui.treeView.setModel(self.proxyModel)
+#        self.ui.treeView.setRootIndex(self.proxyModel)
+
+        [self.ui.treeView.setColumnHidden(cols, True) for cols in range(1,4)]
         self.ui.treeView.clicked.connect(self.onClick)
+        self.ui.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.ui.treeView.customContextMenuRequested.connect(self.create_popup_menu)
+
         self.ui.filedirsearch.textChanged.connect(self.textFilter)
-        [self.ui.severityComboBox.addItem(sevs) for sevs in severity]
         self.ui.actionOpen.triggered.connect(self.openDirDialog)
         self.ui.actionAbout_Qt.triggered.connect(self.aboutQt)
+        self.ui.actionQuit.triggered.connect(self.close)
 
-#        self.ui.txtProfileDesc.textChanged.connect(self.onChange)
-#        self.ui.tabWidget.currentChanged.connect(self.onChange)
         self.ui.tabWidget.tabCloseRequested.connect(self.removeTab)
-
         self.textFilter()
+
+
+    def add_cb(self):
+        print "add callback"
+
+
+    def new_file(self):
+        pass
+
 
     def onClick(self, index):
         path = self.sender().model().filePath(index)
 
         load_yaml_to_gui(self, path)
+
+        self.ui.txtGroupDesc.textChanged.connect(self.onChange)
+        self.ui.txtGroupTitle.textChanged.connect(self.onChange)
 
 
     def openDirDialog(self):
@@ -114,22 +141,37 @@ class ApplicationWindow(QtWidgets. QMainWindow):
             setDirectory(self, user_home())
 
 
+    def create_popup_menu(self, pos):
+        self.popup_menu = QtWidgets.QMenu()
+        self.popup_menu.addAction("New", self.add_cb)
+        self.popup_menu.addAction("Rename", self.add_cb)
+        self.popup_menu.addSeparator()
+        self.popup_menu.addAction("Delete", self.add_cb)
+
+        self.popup_menu.exec_(self.ui.treeView.viewport().mapToGlobal(pos))
+
+
+    def on_context_menu(self, pos):
+        node = self.treeWidget.mapToGlobal(pos)
+        self.popup_menu.exec_(self.treeWidget.mapToGlobal(pos))
+
+
     def textFilter(self):
         regExp = QtCore.QRegExp(self.ui.filedirsearch.text(), QtCore.Qt.CaseInsensitive)
         self.proxyModel.setFilterRegExp(regExp)
 
+
     def removeTab(self, index):
-#        widget = self.ui.tabWidget(index)
-#        if widget is not None:
-#            widget.deleteLater()
         self.ui.tabWidget.removeTab(index)
+
 
     def onChange(self):
         print "text changed"
-        #self.ui.tabWidget.setTabText(1, "test")
+
 
     def aboutQt(self):
         QMessageBox.aboutQt(self)
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
